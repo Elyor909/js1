ebc23e3f19368a3b828a1896bbb92e128309b643
Elyor <nuridinove2001@gmail.com>
2022-12-23 11:06:07 +0500

    js1

---
+++ main.js
@@ -0,0 +1,16 @@
+var myName = prompt('Введите свое имя');
+var age = prompt('Введите свой возраст');
+var primerOne = prompt('Решите пример 6 + 3 = ?');
+var primerTwo = prompt('Решите пример 20 - 5 = ?');
+var primeThree = prompt('Решите пример 15 * 2 = ?');
+var primeFour = prompt('Решите пример 8 / 4 = ?');
+var primerFive = prompt('Решите пример 10 % 3 = ?');
+alert('Откройте консоль для просмотра');
+
+console.log('Меня зовут ' + myName);
+console.log('Мне ' + age);
+console.log('Пример 1: 6 + 3 = ' + (6 + 3) + ' Ваш ответ ' + primerOne);
+console.log('Пример 2: 20 - 5 = ' + (20 - 5) + ' Ваш ответ ' + primerTwo);
+console.log('Пример 3: 15 * 2 = ' + 15 * 2 + ' Ваш ответ ' + primeThree);
+console.log('Пример 4: 8 / 4 = ' + 8 / 4 + ' Ваш ответ ' + primeFour);
+console.log('Пример 5: 10 % 3 = ' + 10 % 3 + ' Ваш ответ ' + primerFive);
